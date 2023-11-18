3b650042590c3611dbbde29260b15e9a94eef7a8
VladimirKrivko <mark.tatyschev@gmail.com>
2023-11-18 17:15:18 +0300

    added cat bean with prototype scope & tests passed successfully

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,11 +2,18 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class App {
+
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld bean1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.printf("Comparison of HelloWorld beans: %b%n", bean1 == bean2);
+        System.out.printf("Comparison of Cat beans: %b%n", cat1 == cat2);
     }
 }
--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -2,13 +2,21 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Scope;
 
+import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
+
 @Configuration
 public class AppConfig {
- 
-    @Bean(name="helloworld")
+
+    @Bean(name = "helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
         helloWorld.setMessage("Hello World!");
         return helloWorld;
+    }
+
+    @Scope(value = SCOPE_PROTOTYPE)
+    @Bean(name = "cat")
+    public Cat getCat() {
+        return new Cat();
     }
 }
